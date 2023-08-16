9f34e10ce9790dd60549851e66b3a3cfd4cac8f5
olatubora-felix <olatubora1996@gmail.com>
2023-08-16 09:41:16 +0100

    Recipe Web app

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: { 'process.env': JSON.stringify(env) },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
