c112ed1457675f84fc7175b6e6dead9c4626b94a
olatubora-felix <olatubora1996@gmail.com>
2023-08-16 08:56:11 +0100

    Recipe Web app

--- src/services/Apis.tsx
+++ src/services/Apis.tsx
@@ -4,7 +4,7 @@ import { getItems, getPosition } from "../utils/helpers";
 
 // Fetch Recipes from API
 export const fetchRecipes = async (slug: string) => {
-  const { data } = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${slug ?? "food"}&app_id=${process?.env.REACT_APP_APP_ID}&app_key=${process?.env.REACT_APP_API_KEY}`);
+  const { data } = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${slug ?? "food"}&app_id=${process?.env.REACT_APP_APP_ID ?? "c008b714"}&app_key=${process?.env.REACT_APP_API_KEY ?? "7a45267f438c9361d04d467749411a79"}`);
   return data?.hits
 }
 
@@ -23,7 +23,7 @@ export const getRecipeById = async () => {
 export const fetchCurrency = async () => {
   const amount = getItems("amount")
   if (amount) {
-    const { data } = await axios.get(`https://v6.exchangerate-api.com/v6/${process.env.REACT_APP_EXCHANGE_KEY}/pair/NGN/USD/${amount ?? ""}`);
+    const { data } = await axios.get(`https://v6.exchangerate-api.com/v6/${process.env.REACT_APP_EXCHANGE_KEY ?? "fe775f211305302e54a2c6a0"}/pair/NGN/USD/${amount ?? ""}`);
     return data
   } else {
     throw Error("Something went wrong")
@@ -38,7 +38,7 @@ export const fetchRestuarants = async (search: string) => {
     latitude: positionObj.coords.latitude,
     longitude: positionObj.coords.longitude,
   };
-  const { data } = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position.latitude},${position.longitude}&radius=1500&type=${search ?? "restaurants"}&key=${process.env.REACT_APP_GOOGLE_KEY}`);
+  const { data } = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position.latitude},${position.longitude}&radius=1500&type=${search ?? "restaurants"}&key=${process.env.REACT_APP_GOOGLE_KEY ?? "AIzaSyChGx3uBGFm3R9aP-JzwCy5B0R_ZTMUv4E"}`);
   return data?.results
 }
 
